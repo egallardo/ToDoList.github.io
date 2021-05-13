68e35d29c82f70623fc7ad09a273a2ccc96cca7c
Elmer Gallardo <elgallardo@gmail.com>
2021-05-12 21:08:43 -0600

    adding css style and layouts

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     var today = new Date();
@@ -24,16 +25,35 @@ app.get("/", function(req, res){
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 });
 
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
-    items.push(item);
+    let item = req.body.newItem;
 
-    res.redirect("/")
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    }else {
+        items.push(item);
+        res.redirect("/")
+    }
 });
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems} )
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.reditect("/work");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
