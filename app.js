3625f89418af7fe375e7e117ca40b975d2d9cff6
Elmer Gallardo <elgallardo@gmail.com>
2023-01-26 19:43:40 -0600

    Deleting items from out ToDoList Database

--- app.js
+++ app.js
@@ -55,15 +55,25 @@ app.get("/", function(req, res){
 
 
 app.post("/", function(req, res){
-    const item = req.body.newItem;
+    const itemName = req.body.newItem;
 
-    if (req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work")
-    }else {
-        items.push(item);
-        res.redirect("/")
-    }
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+    res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndRemove(checkedItemId, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Item deleted");
+        }
+    });
+    res.redirect("/");
 });
 
 app.get("/work", function(req, res){
